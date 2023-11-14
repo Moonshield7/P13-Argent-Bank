524f34a10079d0abf828d41d0cd12975e2922e36
Moonshield7 <fannie.p@hotmail.fr>
2023-11-14 13:22:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyRouter from './utils/MyRouter'
+
+function App() {
+
+  return (
+    <div>
+      <MyRouter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home () {
+  return (
+    <div>
+      <h1>Honk</h1>
+    </div>
+  )
+}
+
+export default Home;
