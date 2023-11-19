0b00bb36687e8da7dcf7b80a626e7f0648c0efbc
Moonshield7 <fannie.p@hotmail.fr>
2023-11-19 13:09:30 +0100

    💄 Creating User page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import User from "./pages/User";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />}  />
+        <Route path='/user' element={<User />}  />
         <Route path='/sign-in' element={<SignIn />}  />
       </Routes>
     </Router>
---
+++ src/pages/User/index.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import UserContainer from "../../components/UserContainer";
+
+function User () {
+  return (
+    <div>
+      <Header />
+      <UserContainer />
+      <Footer />
+    </div>
+  )
+}
+
+export default User;
