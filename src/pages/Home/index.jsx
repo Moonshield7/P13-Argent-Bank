5b54d3353bfe7a182dee01dd0d747fccdc0deefb
Moonshield7 <fannie.p@hotmail.fr>
2023-11-19 12:03:53 +0100

    💄 Creating Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,15 @@
+import Banner from "../../components/Banner";
+import Features from "../../components/Features";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function Home () {
   return (
     <div>
       <Header />
+      <Banner />
+      <Features />
+      <Footer />
     </div>
   )
 }
