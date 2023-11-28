9e54494d79c5ff5a30471527eabee7f4ae6f66f1
Moonshield7 <fannie.p@hotmail.fr>
2023-11-28 12:16:47 +0100

    ✨ Added sign in feature

--- src/components/SignInContainer/index.jsx
+++ src/components/SignInContainer/index.jsx
@@ -1,22 +1,57 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login } from "../../utils/features/users";
+import { useState } from "react";
+import { fetchLogin, fetchUser } from "../../services/api.js"
+import { useNavigate } from "react-router";
 
 function SignInContainer () {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorLogin, setErrorLogin] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const verifyUser = await fetchLogin(email, password)
+     
+    if(verifyUser.status === 200){
+      setErrorLogin(false)
+      const userData = await fetchUser(verifyUser.body.token);
+      console.log(userData)
+      dispatch(login({
+        email: email,
+        password: password,
+        firstname: userData.firstName,
+        lastname: userData.lastName,
+        loggedIn: true,
+        token: verifyUser.body.token,
+      }));
+      navigate('/profile')
+    }
+    if(verifyUser.status === 400){
+      setErrorLogin(true);
+    }
+  }
+
   return (
     <main className="main bg-dark" style={{minHeight: "75vh", paddingTop: "3rem"}}>
     <section className="sign-in-content">
       <i className="fa fa-user-circle sign-in-icon"></i>
       <h1>Sign In</h1>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="input-wrapper">
-          <label htmlFor="username">Username</label><input type="text" id="username" />
+          <label htmlFor="username">Email</label><input type="text" id="username" onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="input-wrapper">
-          <label htmlFor="password">Password</label><input type="password" id="password" />
+          <label htmlFor="password">Password</label><input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div className="input-remember">
           <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
         </div>
-        <Link to="/profile" className="sign-in-button">Sign In</Link>
+        {errorLogin === false ? "" : <div className="login-error">Email ou mot de passe incorrect</div>}
+        <button type="submit" className="sign-in-button">Sign In</button>
       </form>
     </section>
   </main>
