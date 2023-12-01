7792b016c1c4fae66fd0c889f02004af592e7e02
Moonshield7 <fannie.p@hotmail.fr>
2023-12-01 15:16:24 +0100

    🎨 Fixing modifyUser issue when inputs empty

--- src/components/UserHeader/index.jsx
+++ src/components/UserHeader/index.jsx
@@ -5,30 +5,29 @@ import { modifyUser } from "../../services/api";
 
 function UserHeader () {
   const user = useSelector(selectUser);
-  const [modifyError, setModifyError] = useState(false);
+  const [modifyError, setModifyError] = useState();
   const dispatch = useDispatch();
   const [editing, setEditing] = useState(false);
 
     function toggleEditUser () {
       setEditing(!editing);
+      setModifyError(false)
     }
 
     async function editUser () {
       const firstNameInput = document.getElementById("firstNameInput");
       const lastNameInput = document.getElementById("lastNameInput");
-      if(firstNameInput.value || lastNameInput.value){      
-        if(firstNameInput.value) {
-          dispatch(editFirstname(firstNameInput.value));
-        }
-        if(lastNameInput.value) {
-          dispatch(editLastname(lastNameInput.value))
-        }
-        
-        const editUser = await modifyUser(user.token, firstNameInput.value, lastNameInput.value).catch(setModifyError(true));
+      if(firstNameInput.value && lastNameInput.value){    
+        setModifyError(false);  
+        dispatch(editFirstname(firstNameInput.value));
+        dispatch(editLastname(lastNameInput.value))
+
+        const editUser = await modifyUser(user.token, firstNameInput.value, lastNameInput.value);
         toggleEditUser();
         setModifyError(false);
-      }
 
+      }
+      setModifyError(true)
       
     }
 
@@ -37,13 +36,13 @@ function UserHeader () {
       <div className="header">
         <h1>Welcome back</h1>
         <div className="edit-user">
-          <input className="edit-user-input" id="firstNameInput" type="text" placeholder={user.firstname}></input>
-          <input className="edit-user-input" id="lastNameInput" type="text" placeholder={user.lastname}></input>
+          <input className="edit-user-input" id="firstNameInput" type="text" minLength={1} defaultValue={user.firstname}></input>
+          <input className="edit-user-input" id="lastNameInput" type="text" minLength={1} defaultValue={user.lastname}></input>
         </div>
         <div className="edit-user">
           <button className="edit-user-button" onClick={editUser}>Save</button>
           <button className="edit-user-button" onClick={toggleEditUser}>Cancel</button>
-          {modifyError ? <div className="">Erreur lors de la modification (connexion avec l'API), réessayez plus tard.</div> : ""}
+          {modifyError ? <div className="">Veuillez remplir les champs correctement.</div> : ""}
         </div>
 
       </div>
